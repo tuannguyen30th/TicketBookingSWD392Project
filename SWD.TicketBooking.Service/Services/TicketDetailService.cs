08d515cbd15b295cfed74dee6ac89b73a21a237a
tivoo <tienvtmse170222@fpt.edu.vn>
2024-06-21 02:22:02 -0700

    fix

--- SWD.TicketBooking.Service/Services/TicketDetailService.cs
+++ SWD.TicketBooking.Service/Services/TicketDetailService.cs
@@ -154,16 +154,17 @@ namespace SWD.TicketBooking.Service.Services
             try
             {
                 var checkUser = await _userRepo.GetAll().Where(u => u.Email.Equals(email)).Select(u => u.UserID).FirstOrDefaultAsync();
+                var searchTicket = new SearchTicketModel();
                 if (checkUser == Guid.Empty)
                 {
-                    return null;
+                    return searchTicket;
                 }
                 else
                 {
                     var check = await _bookingRepo.GetAll().Where(b => b.QRCode.Equals(QRCode) && b.UserID.Equals(checkUser)).Select(b => b.BookingID).FirstAsync();
                     if (check == null)
                     {
-                        throw new NotFoundException("Not found qr code!");
+                        return searchTicket;
                     }
                     else
                     {
