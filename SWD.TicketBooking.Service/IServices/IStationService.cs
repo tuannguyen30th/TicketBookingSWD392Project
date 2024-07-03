5d10caba9f402123fd26b655cb678051b135f0a5
1903giabao <baogavip00@gmail.com>
2024-07-03 16:11:08 +0700

    GetAllStationByCompany

--- SWD.TicketBooking.Service/IServices/IStationService.cs
+++ SWD.TicketBooking.Service/IServices/IStationService.cs
@@ -16,5 +16,7 @@ namespace SWD.TicketBooking.Service.IServices
         Task<string> UpdateStation(Guid stationId, CreateStationModel stationModel);
         Task<List<StationFromRouteModel>> GetAllStationInRoute(Guid id);
         Task<bool> CreateStationWithService(CreateStationWithServiceModel stationModel);
+        Task<List<StationFromRouteModel>> GetAllStationsByCompanyID(Guid companyID);
+
     }
 }
