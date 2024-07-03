cb9e627f48177fb5f77f29e4a4459d833f3b13ea
Ngoc Tuan <tuannnse170112@fpt.edu.vn>
2024-07-03 22:05:38 +0700

    fix-response

--- SWD.TicketBooking.Service/Dtos/StationInSearchTicket.cs
+++ SWD.TicketBooking.Service/Dtos/StationInSearchTicket.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SWD.TicketBooking.Service.Dtos
 {
     public class ServiceInSearchTicket
     {
+        [JsonPropertyName("ServiceName")]
         public string? ServiceName { get; set; }
+        [JsonPropertyName("Price")]
         public double Price { get; set; }
 
     }
