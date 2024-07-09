941898cf8ad26b62458d61436612981d6a4286c5
1903giabao <baogavip00@gmail.com>
2024-07-09 16:33:04 +0700

    GetPopularRoute

---
+++ SWD.TicketBooking.Service/Dtos/PopularRouteModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.TicketBooking.Service.Dtos
+{
+    public class PopularRouteModel
+    {
+        public Guid RouteID { get; set; }
+        public string FromCity { get; set; }
+        public string ToCity { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+        public int TotalBooking { get; set; }
+    }
+}
