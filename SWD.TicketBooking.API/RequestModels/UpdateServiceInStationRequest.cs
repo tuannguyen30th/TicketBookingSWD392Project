b64191b2620c93230e7464262f1e25e917dbe00b
1903giabao <baogavip00@gmail.com>
2024-07-12 01:22:06 +0700

    UpdateServiceStation

--- SWD.TicketBooking.API/RequestModels/UpdateServiceInStationRequest.cs
+++ SWD.TicketBooking.API/RequestModels/UpdateServiceInStationRequest.cs
@@ -2,9 +2,6 @@
 {
     public class UpdateServiceInStationRequest
     {
-        public Guid Station_ServiceID { get; set; }
-        public Guid StationID { get; set; }
-        public Guid ServiceID { get; set; }
         public double Price { get; set; }
         public IFormFile ImageUrl { get; set; }
     }
