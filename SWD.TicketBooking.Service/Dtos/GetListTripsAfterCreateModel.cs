bed63d0257bba78e2038b2c37eadeb64a643abae
Ngoc Tuan <tuannnse170112@fpt.edu.vn>
2024-07-14 03:56:48 +0700

    commit

---
+++ SWD.TicketBooking.Service/Dtos/GetListTripsAfterCreateModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWD.TicketBooking.Service.Dtos
+{
+    public class GetListTripsAfterCreateModel
+    {
+        public Guid? TripID { get; set; }
+        public Guid? Route_CompanyID { get; set; }
+        public bool? IsTemplate { get; set; }
+        public Guid? StaffID { get; set; }
+        public Guid? TemplateID { get; set; }
+        public GetTimeTripAfterCreateModel GetTimeTripAfterCreateModels { get; set; } = new GetTimeTripAfterCreateModel();
+        public class GetTimeTripAfterCreateModel
+        {
+            public DateTime? StartTime { get; set; }
+            public DateTime? EndTime { get; set; }
+        }
+      
+    }
+}
---
+++ SWD.TicketBooking.Service/Dtos/GetTripAfterCreateModel.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SWD.TicketBooking.Service.Dtos
+{
+    public class GetTripAfterCreateModel
+    {
+        [JsonPropertyName("TripID")]
+        public Guid TripID { get; set; }
+
+        [JsonPropertyName("FromCity")]
+        public string? FromCity { get; set; }
+
+        [JsonPropertyName("ToCity")]
+        public string? ToCity { get; set; }
+
+        [JsonPropertyName("StartLocation")]
+        public string? StartLocation { get; set; }
+
+        [JsonPropertyName("EndLocation")]
+        public string? EndLocation { get; set; }
+
+        [JsonPropertyName("StartTime")]
+        public string? StartTime { get; set; }
+
+        [JsonPropertyName("EndTime")]
+        public string? EndTime { get; set; }
+
+        [JsonPropertyName("StartDate")]
+        public string? StartDate { get; set; }
+
+        [JsonPropertyName("EndDate")]
+        public string? EndDate { get; set; }
+
+        [JsonPropertyName("StaffName")]
+        public string? StaffName { get; set; }
+
+        [JsonPropertyName("MinPrice")]
+        public double? MinPrice { get; set; }
+
+        [JsonPropertyName("MaxPrice")]
+        public double? MaxPrice { get; set; }
+
+        [JsonPropertyName("Status")]
+        public string? Status { get; set; }
+    }
+}
