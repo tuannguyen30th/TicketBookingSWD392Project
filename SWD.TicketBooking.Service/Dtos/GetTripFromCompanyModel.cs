052491ee49173e5e3529e9fb48ab80da8c8051df
Ngoc Tuan <tuannnse170112@fpt.edu.vn>
2024-07-16 02:24:57 +0700

    fix-responseTripFromCompany

--- SWD.TicketBooking.Service/Dtos/GetTripFromCompanyModel.cs
+++ SWD.TicketBooking.Service/Dtos/GetTripFromCompanyModel.cs
@@ -11,32 +11,43 @@ namespace SWD.TicketBooking.Service.Dtos
     {
         [JsonPropertyName("TripID")]
         public Guid TripID { get; set; }
+
         [JsonPropertyName("StaffID")]
         public Guid StaffID { get; set; }
-        [JsonPropertyName("FromCity")]
+
+        [JsonPropertyName("StaffName")]
         public string? StaffName { get; set; }
-        [JsonPropertyName("MinPrice")]
+
+        [JsonPropertyName("FromCity")]
         public string? FromCity { get; set; }
+
         [JsonPropertyName("ToCity")]
         public string? ToCity { get; set; }
+
         [JsonPropertyName("StartLocation")]
         public string? StartLocation { get; set; }
+
         [JsonPropertyName("EndLocation")]
         public string? EndLocation { get; set; }
+
         [JsonPropertyName("StartTime")]
         public string? StartTime { get; set; }
+
         [JsonPropertyName("EndTime")]
         public string? EndTime { get; set; }
+
         [JsonPropertyName("StartDate")]
         public string? StartDate { get; set; }
+
         [JsonPropertyName("EndDate")]
         public string? EndDate { get; set; }
 
-        [JsonPropertyName("StaffName")]
-        
+        [JsonPropertyName("MinPrice")]
         public double? MinPrice { get; set; }
+
         [JsonPropertyName("MaxPrice")]
         public double? MaxPrice { get; set; }
+
         [JsonPropertyName("Status")]
         public string? Status { get; set; }
     }
